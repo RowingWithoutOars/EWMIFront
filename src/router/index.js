9349415d8046732cf6fc2ce450b9abc42a681244
RowingWithoutOars <2856431415@qq.com>
2019-01-05 16:24:42 +0800

    修复无法build的bug

--- src/router/index.js
+++ src/router/index.js
@@ -46,47 +46,47 @@ const routes = [
             component: updateData,
             meta: ['修改数据', '修改数据'],
 		}
-		// ,{
-         //    path: '/updateData',
-         //    component: addData,
-         //    meta: ['添加数据', '添加商品'],
-        // },{
-         //    path: '/fyzwlist',
-         //    component: fyzwlist,
-         //    meta: ['浮游植物', '浮游植物'],
-		// },{
-         //    path: '/fydwlist',
-         //    component: fyzwlist,
-         //    meta: ['浮游动物', '浮游动物'],
-        // },{
-         //    path: '/dqlist',
-         //    component: dqlist,
-         //    meta: ['底栖生物', '底栖生物'],
-        // },{
-         //    path: '/wswqllist',
-         //    component: wswqllist,
-         //    meta: ['微生物群落', '微生物群落'],
-        // },{
-         //    path: '/dnsjlist',
-         //    component: dnsjlist,
-         //    meta: ['底泥数据', '底泥数据'],
-        // },{
-		// 	path: '/visitor',
-		// 	component: visitor,
-		// 	meta: ['图表', '用户分布'],
-		// },{
-         //    path: '/statistic',
-         //    component: statistic,
-         //    meta: ['图表', '用户分布'],
-        // },{
-		// 	path: '/adminSet',
-		// 	component: adminSet,
-		// 	meta: ['设置', '管理员设置'],
-		// },{
-		// 	path: '/explain',
-		// 	component: explain,
-		// 	meta: ['说明', '说明'],
-		// }
+		,{
+            path: '/updateData',
+            component: addData,
+            meta: ['添加数据', '添加商品'],
+        },{
+            path: '/fyzwlist',
+            component: fyzwlist,
+            meta: ['浮游植物', '浮游植物'],
+		},{
+            path: '/fydwlist',
+            component: fyzwlist,
+            meta: ['浮游动物', '浮游动物'],
+        },{
+            path: '/dqlist',
+            component: dqlist,
+            meta: ['底栖生物', '底栖生物'],
+        },{
+            path: '/wswqllist',
+            component: wswqllist,
+            meta: ['微生物群落', '微生物群落'],
+        },{
+            path: '/dnsjlist',
+            component: dnsjlist,
+            meta: ['底泥数据', '底泥数据'],
+        },{
+			path: '/visitor',
+			component: visitor,
+			meta: ['图表', '用户分布'],
+		},{
+            path: '/statistic',
+            component: statistic,
+            meta: ['图表', '用户分布'],
+        },{
+			path: '/adminSet',
+			component: adminSet,
+			meta: ['设置', '管理员设置'],
+		},{
+			path: '/explain',
+			component: explain,
+			meta: ['说明', '说明'],
+		}
 		]
 	}
 ]
