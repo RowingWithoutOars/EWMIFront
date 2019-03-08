2f12b22cb069f61f1579cbf9ee96ffb7fc53ad53
RowingWithoutOars <2856431415@qq.com>
2019-03-08 19:09:56 +0800

    引入地图部分数据

---
+++ src/map.js
@@ -0,0 +1,11 @@
+export function MP() {
+    return new Promise(function (resolve, reject) {
+        window.onload = function () {
+            resolve(BMap)
+        }
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "http://api.map.baidu.com/api?v=2.0&ak=piXQ5CgHFqEefqCVbhhBFfe6HjF7l4zW&callback=init";
+        script.onerror = reject; document.head.appendChild(script);
+    })
+}
